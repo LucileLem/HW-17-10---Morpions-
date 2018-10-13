43391f8275f4e3477943514dcec47f950eca00a7
MrDorak <dorianturle@hotmail.fr>
2018-10-14 01:59:34 +0200

    fixed the game so it actually works as it should

--- Main.js
+++ Main.js
@@ -1,51 +1,70 @@
-var tourDuJoueur1 = true;
-var partieGagnee = false;
-var cells = document.querySelectorAll('.cell');
+var tour_du_joueur1 = 0;
+const cells = document.querySelectorAll('.cell');
+const gagnant_text = document.getElementById('gagnant');
+const restart = document.getElementById('restart');
 
-var afficherSymbole = function(cell) {
-	// a remplir
-	// 1 - verifier case remplie ou pas
-	if (cell.textContent === '') {
-		// 2 - poser symbole J1 ou j2
-		if (tourDuJoueur1) {
-			cell.textContent = 'X';
-		} else {
-			cell.textContent = 'O';
-		}
-		// 4 - changer le joueur courant
-		tourDuJoueur1 = !tourDuJoueur1;
+cells.forEach(cell => cell.addEventListener('click', jouer));
+restart.addEventListener('click', restartGame);
+
+function jouer() {
+// 1 - verifier case remplie ou pas
+	if (!this.innerHTML) {
+		this.innerHTML = tour();
+	}
+	// minimum 5 tours avant d'avoir un gagnant
+	if (tour_du_joueur1 >= 5) {
+		verifierCombinaisons()
+	}
+}
+
+// 2 - poser symbole ; si tour_du_joueur est un multiple de 2 c'est le joueur 2 qui vient de jouer
+function tour() {
+	tour_du_joueur1++;
+	return tour_du_joueur1 % 2 === 0 ? 'O' : 'X';
+};
+
+function afficherGagnant(gagnant){
+	console.log('WIN');	
+	gagnant_text.style.display = 'block';
+	gagnant_text.innerHTML = 'Bravo ' + (gagnant === 'X' ? 'joueur 1' : 'joueur 2') + '!';
+	restart.style.display = 'block';
+	cells.forEach(cell => cell.innerHTML = '');
+}
+
+	// 3 - check combinaison gagnante
+function verifierCombinaisons() {
+	//horizontales
+	if (cells[0].innerHTML !== '' && cells[0].innerHTML === cells[1].innerHTML && cells[1].innerHTML === cells[2].innerHTML) {
+		afficherGagnant(cells[0].innerHTML);
+	}
+	else if (cells[3].innerHTML !== '' && cells[3].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[5].innerHTML) {
+		afficherGagnant(cells[3].innerHTML);
+	}
+	else if (cells[6].innerHTML !== '' && cells[6].innerHTML === cells[7].innerHTML && cells[7].innerHTML === cells[8].innerHTML) {
+		afficherGagnant(cells[6].innerHTML);
+	}
+	// verticales 
+	else if (cells[0].innerHTML !== '' && cells[0].innerHTML === cells[3].innerHTML && cells[3].innerHTML === cells[6].innerHTML) {
+		afficherGagnant(cells[0].innerHTML);
+	}
+	else if (cells[1].innerHTML !== '' && cells[1].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[7].innerHTML) {
+		afficherGagnant(cells[1].innerHTML);
+	}
+	else if (cells[2].innerHTML !== '' && cells[2].innerHTML === cells[5].innerHTML && cells[5].innerHTML === cells[8].innerHTML) {
+		afficherGagnant(cells[2].innerHTML);
+	}
+	// diagonales 
+	else if (cells[0].innerHTML !== '' && cells[0].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[8].innerHTML) {
+		afficherGagnant(cells[0].innerHTML);
+	}
+	else if (cells[2].innerHTML !== '' && cells[2].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[6].innerHTML) {
+		afficherGagnant(cells[2].innerHTML);
 	}
 };
 
-var combinaisons = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
-
-var verifierCombinaisons = function() {
-	// a remplir
-	// 3 - check combinaison gagnante
-	combinaisons.forEach(function(combinaison) {
-		if (
-			cells[combinaison[0]].textContent === cells[combinaison[1]].textContent &&
-			cells[combinaison[1]].textContent === cells[combinaison[2]].textContent &&
-			cells[combinaison[0]].textContent !== ''
-		) {
-			console.log('WIN');
-			var currentPlayer;
-			if (tourDuJoueur1) {
-				currentPlayer = 'joueur 2';
-			} else {
-				currentPlayer = 'joueur 1';
-			}
-			alert('Bravo ' + currentPlayer + '!');
-			partieGagnee = true;
-		}
-	});
-};
-
-cells.forEach(function(cell) {
-	cell.addEventListener('click', function() {
-		if (!partieGagnee) {
-			afficherSymbole(cell);
-			verifierCombinaisons();
-		}
-	});
-});
+// pour rajouter un peu de durée de vie !
+function restartGame(){
+	gagnant.style.display = 'none';
+	restart.style.display = 'none';
+	tour_du_joueur1 = 0;
+}
